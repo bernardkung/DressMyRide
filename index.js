09bbacca038e6f6a333371bf69bfeec97f4f64c2
EC2 Default User <ec2-user@ip-172-31-92-163.ec2.internal>
2020-11-12 07:01:49 +0000

    Ribbon mouseover implemented

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res)=>{
+    res.render("index.ejs");
+});
+
+
+app.listen(process.env.PORT, process.env.IP, () => {
+    console.log("The server has started!");
+})

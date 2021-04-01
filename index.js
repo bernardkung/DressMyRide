bab2969f173b8f21a088f53b7ebf044f4e83efa0
Bernard Kung <bpkung@gmail.com>
2021-04-01 22:25:39 +0000

    location search

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
-const express = require('express');
-const app = express();
+const express   = require('express');
+const app       = express();
+const cities    = require(__dirname + '/public/assets/cities.json')
 
+// Add public directory
 app.use(express.static(__dirname + '/public'));
+// Enable bodyparsing
+app.use(express.urlencoded());
 app.set("view engine", "ejs");
 
+
+// Set routes
 app.get("/", (req, res)=>{
     res.render("index.ejs");
 });
 
+app.get('/template', (req, res)=>{
+    res.render("template.ejs")
+})
+
+
+
+
+app.get('/location', (req, res)=>{
+    res.render("location.ejs", {location: null, cities: cities})
+})
+
+app.post('/location/', (req, res)=>{
+    res.render("location.ejs", {location: req.body.location, cities: cities})
+})
 
 app.listen(process.env.PORT, process.env.IP, () => {
     console.log("The server has started!");

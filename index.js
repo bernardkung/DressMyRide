6b23aba5c3e7ca4f69b0b47d10174fd1d19a20a9
Bernard Kung <bpkung@gmail.com>
2021-04-08 01:49:52 +0000

    recommender is working

--- index.js
+++ index.js
@@ -10,24 +10,16 @@ app.set("view engine", "ejs");
 
 
 // Set routes
-app.get("/", (req, res)=>{
-    res.render("index.ejs");
-});
 
-app.get('/template', (req, res)=>{
-    res.render("template.ejs")
+app.get('/', (req, res)=>{
+    res.render("index.ejs", {location: null, cities: cities})
+})
+
+app.post('/', (req, res)=>{
+    res.render("index.ejs", {location: req.body.location, cities: cities})
 })
 
 
-
-
-app.get('/location', (req, res)=>{
-    res.render("location.ejs", {location: null, cities: cities})
-})
-
-app.post('/location/', (req, res)=>{
-    res.render("location.ejs", {location: req.body.location, cities: cities})
-})
 
 app.listen(process.env.PORT, process.env.IP, () => {
     console.log("The server has started!");
